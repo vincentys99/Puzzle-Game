88b762a7cd28a8a04bb9a6bc92df2a0f3dc2a5a3
Eric Lim <ysfung1704@gmail.com>
2021-09-21 15:49:19 +0800

    Testing rendering tilemap

---
+++ core/assets/tilemap/map0.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="map0" tilewidth="32" tileheight="32" tilecount="132" columns="12">
+ <image source="map0.png" width="402" height="379"/>
+</tileset>
--- core/src/com/chuchu/puzzlegame/GameScreen2.java
+++ core/src/com/chuchu/puzzlegame/GameScreen2.java
@@ -1,18 +1,21 @@
 package com.chuchu.puzzlegame;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 public class GameScreen2 implements Screen {
 
     final PuzzleGame game;
 
+    Viewport viewport;
     OrthographicCamera camera;
     OrthogonalTiledMapRenderer tiledMapRenderer;
     TiledMap tiledMap;
@@ -22,9 +25,11 @@ public class GameScreen2 implements Screen {
 
         camera = new OrthographicCamera();
         camera.setToOrtho(false, 1920, 1080);
+        viewport = new FitViewport(1920, 1080, camera);
 
-        tiledMap = new TmxMapLoader().load("map0.tmx");
-        tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
+        tiledMap = new TmxMapLoader().load("tilemap/test_map0.tmx");
+//        tiledMap = new TmxMapLoader().load("tilemap_test/template.tmx");
+        tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap, 2);
     }
 
     @Override
@@ -34,22 +39,23 @@ public class GameScreen2 implements Screen {
 
     @Override
     public void render(float delta) {
-//        ScreenUtils.clear(0, 0, 0.2f, 1);
-
-        Gdx.gl.glClearColor(1, 0, 0, 1);
-        Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        ScreenUtils.clear(0, 0, 0.2f, 1);
 
         camera.update();
         game.batch.setProjectionMatrix(camera.combined);
 
         tiledMapRenderer.setView(camera);
         tiledMapRenderer.render();
+
+        if (Gdx.input.isKeyPressed(Input.Keys.ESCAPE)) {
+            game.setScreen(new MainMenuScreen(game));
+            dispose();
+        }
     }
 
     @Override
     public void resize(int width, int height) {
-
+        viewport.update(width, height);
     }
 
     @Override
