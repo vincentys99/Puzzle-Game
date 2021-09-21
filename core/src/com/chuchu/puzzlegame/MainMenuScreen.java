93aecacd31bf25e9838a64feaa4e39fd27b0749a
Eric Lim <ysfung1704@gmail.com>
2021-09-21 14:01:22 +0800

    Cleaning up codes

--- core/src/com/chuchu/puzzlegame/MainMenuScreen.java
+++ core/src/com/chuchu/puzzlegame/MainMenuScreen.java
@@ -16,45 +16,57 @@ import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.Timer;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 public class MainMenuScreen implements Screen {
+
     final PuzzleGame game;
-    OrthographicCamera camera;
-    Texture texture;
-    Stage stage;
-    TextButton btnStart;
-    TextButton btnOption;
-    TextButton btnExit;
-    //TextField optionWindow;
-    TextButtonStyle textButtonStyle;
-    Table menuTable;
-    Table optionTable;
-    Music backgroundMusic;
-    Sound btnClickSound;
-    Skin defaultSkin;
+    private OrthographicCamera camera;
+    private Sound btnClickSound;
+    private Music backgroundMusic;
+    private Texture texture;
+    private Stage stage;
+    private TextButtonStyle textButtonStyle;
+    private TextButton btnStart;
+    private TextButton btnOption;
+    private TextButton btnExit;
+    private Table menuTable;
+    private Table optionTable;
+    private Skin defaultSkin;
+//    private TextField optionWindow;
+
     public MainMenuScreen (final PuzzleGame game) {
         this.game = game;
+
+        // Camera
         camera = new OrthographicCamera();
         camera.setToOrtho(false, 1920, 1080);
 
+        // Sound & Music
         btnClickSound = Gdx.audio.newSound(Gdx.files.internal("button_click.mp3"));
         backgroundMusic = Gdx.audio.newMusic(Gdx.files.internal("ZEDD x VALORANT Music Theme.ogg"));
         backgroundMusic.setLooping(true);
 
+        // Image
         texture = new Texture(Gdx.files.internal("valorant_wallpaper_yoru.jpg"));
 
+
+        // Skin
+        defaultSkin = new Skin(Gdx.files.internal("uiskin.json"));
+
+        // Stage for buttons
         stage = new Stage(new ScreenViewport());
         Gdx.input.setInputProcessor(stage);
 
+        // Buttons
         textButtonStyle = new TextButtonStyle();
         textButtonStyle.font = game.font;
         textButtonStyle.overFontColor = Color.RED;
-        this.defaultSkin = new Skin(Gdx.files.internal("uiskin.json"));
+
         btnStart = new TextButton("Start Game", textButtonStyle);
         btnOption = new TextButton("Option", textButtonStyle);
         btnExit = new TextButton("Exit Game", textButtonStyle);
 
-        btnStart.addListener(new ChangeListener() {
+        btnStart.addListener(new ClickListener() {
             @Override
-            public void changed (ChangeEvent event, Actor actor) {
+            public void clicked(InputEvent event, float x, float y) {
                 btnClickSound.play();
                 Timer.schedule(new Timer.Task(){
                     @Override
@@ -66,17 +78,17 @@ public class MainMenuScreen implements Screen {
             }
         });
 
-        btnOption.addListener(new ChangeListener() {
+        btnOption.addListener(new ClickListener() {
             @Override
-            public void changed (ChangeEvent event, Actor actor) {
+            public void clicked(InputEvent event, float x, float y) {
                 btnClickSound.play();
                 loadOptions();
             }
         });
 
-        btnExit.addListener(new ChangeListener() {
+        btnExit.addListener(new ClickListener() {
             @Override
-            public void changed (ChangeEvent event, Actor actor) {
+            public void clicked(InputEvent event, float x, float y) {
                 btnClickSound.play();
                 Timer.schedule(new Timer.Task(){
                     @Override
@@ -87,10 +99,8 @@ public class MainMenuScreen implements Screen {
                 }, 0.5f);
             }
         });
-        optionTable = new Table();
-        optionTable.setX((float)Gdx.graphics.getWidth() / 2);
-        optionTable.setY((float)Gdx.graphics.getHeight() / 2);
 
+        // Menu Table
         menuTable = new Table();
         menuTable.add(btnStart);
         menuTable.row();
@@ -99,6 +109,11 @@ public class MainMenuScreen implements Screen {
         menuTable.add(btnExit);
         menuTable.setFillParent(true);
         stage.addActor(menuTable);
+
+        // Option Table
+        optionTable = new Table();
+        optionTable.setX((float)Gdx.graphics.getWidth() / 2);
+        optionTable.setY((float)Gdx.graphics.getHeight() / 2);
     }
 
     @Override
@@ -115,8 +130,6 @@ public class MainMenuScreen implements Screen {
 
         game.batch.begin();
         game.batch.draw(texture, 0, 0);
-//        game.font.draw(game.batch, "Welcome to the Puzzle Game!", 100, 150);
-
         game.batch.end();
 
         stage.act();
@@ -124,7 +137,6 @@ public class MainMenuScreen implements Screen {
     }
 
     private void loadOptions() {
-
         final TextField resolutionWidth = new TextField("1920", defaultSkin);
         final TextField resolutionHeight = new TextField("1080", defaultSkin);
         TextButton apply = new TextButton("Apply", defaultSkin);
@@ -147,18 +159,19 @@ public class MainMenuScreen implements Screen {
         stage.addActor(optionTable);
 
     }
+
     @Override
     public void resize(int width, int height) {
         stage.getViewport().update(width, height);
         stage.getCamera().position.set((float)Gdx.graphics.getWidth() / 2, (float)Gdx.graphics.getHeight() / 2, 0);
-        //System.out.println(optionTable.getX());
-        //System.out.println(Gdx.graphics.getWidth());
-        //System.out.println((optionTable.getX() / Gdx.graphics.getWidth()) * 100);
-        //System.out.println((optionTable.getY() / Gdx.graphics.getHeight()) * 100);
-        //System.out.println((((float) 50 / 100) * Gdx.graphics.getWidth()));
-       // System.out.println((optionTable.getY() / Gdx.graphics.getHeight()) * 100);
-        this.optionTable.setX(((float) 50 / 100) * Gdx.graphics.getWidth());
-        this.optionTable.setY(((float) 50 / 100) * Gdx.graphics.getHeight());
+//        System.out.println(optionTable.getX());
+//        System.out.println(Gdx.graphics.getWidth());
+//        System.out.println((optionTable.getX() / Gdx.graphics.getWidth()) * 100);
+//        System.out.println((optionTable.getY() / Gdx.graphics.getHeight()) * 100);
+//        System.out.println((((float) 50 / 100) * Gdx.graphics.getWidth()));
+//        System.out.println((optionTable.getY() / Gdx.graphics.getHeight()) * 100);
+        optionTable.setX(((float) 50 / 100) * Gdx.graphics.getWidth());
+        optionTable.setY(((float) 50 / 100) * Gdx.graphics.getHeight());
     }
 
     @Override
@@ -178,10 +191,10 @@ public class MainMenuScreen implements Screen {
 
     @Override
     public void dispose() {
-        texture.dispose();
-        stage.dispose();
         btnClickSound.dispose();
         backgroundMusic.dispose();
+        texture.dispose();
+        stage.dispose();
+        defaultSkin.dispose();
     }
-
 }
