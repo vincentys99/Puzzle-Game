7e8c655b12c6860043e4f22ea5e7890ed515477d
Shiho <shihohohohohohoho@gmail.com>
2021-09-23 09:15:08 +0800

    volume changes as u drag slider and close options button temporarily

--- core/src/com/chuchu/puzzlegame/Screens/Room1.java
+++ core/src/com/chuchu/puzzlegame/Screens/Room1.java
@@ -4,16 +4,27 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.chuchu.puzzlegame.PuzzleGame;
 
 public class Room1 implements Screen {
     final PuzzleGame game;
-
+    private static final int FRAME_COLS = 5, FRAME_ROWS = 1;
     OrthographicCamera camera;
     Music backgroundMusic;
+    Animation<TextureRegion> walkAnimation; // Must declare frame type (TextureRegion)
+    Texture walkSheet;
+    SpriteBatch spriteBatch;
 
+    // A variable for tracking elapsed time for the animation
+    float stateTime;
+    private int xPos = 0;
     public Room1(final PuzzleGame game) {
         this.game = game;
 
@@ -23,6 +34,34 @@ public class Room1 implements Screen {
 
         camera = new OrthographicCamera();
         camera.setToOrtho(false, PuzzleGame.defaultWidth, PuzzleGame.defaultHeight);
+
+        walkSheet = new Texture(Gdx.files.internal("cc.png"));
+
+        // Use the split utility method to create a 2D array of TextureRegions. This is
+        // possible because this sprite sheet contains frames of equal size and they are
+        // all aligned.
+        TextureRegion[][] tmp = TextureRegion.split(walkSheet,
+                walkSheet.getWidth() / FRAME_COLS,
+                walkSheet.getHeight() / FRAME_ROWS);
+
+        // Place the regions into a 1D array in the correct order, starting from the top
+        // left, going across first. The Animation constructor requires a 1D array.
+        TextureRegion[] walkFrames = new TextureRegion[FRAME_COLS * FRAME_ROWS];
+        int index = 0;
+        for (int i = 0; i < FRAME_ROWS; i++) {
+            for (int j = 0; j < FRAME_COLS; j++) {
+                walkFrames[index++] = tmp[i][j];
+            }
+        }
+
+        // Initialize the Animation with the frame interval and array of frames
+        walkAnimation = new Animation<TextureRegion>(0.1f, walkFrames);
+
+        // Instantiate a SpriteBatch for drawing and reset the elapsed animation
+        // time to 0
+        spriteBatch = new SpriteBatch();
+        stateTime = 0f;
+
     }
 
     @Override
@@ -32,20 +71,15 @@ public class Room1 implements Screen {
 
     @Override
     public void render(float delta) {
-        ScreenUtils.clear(0, 0, 0.2f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT); // Clear screen
+        stateTime += Gdx.graphics.getDeltaTime(); // Accumulate elapsed animation time
 
-        camera.update();
-
-        game.batch.setProjectionMatrix(camera.combined);
-
-        game.batch.begin();
-        game.font.draw(game.batch, "Welcome, Player!", 100, 150);
-        game.batch.end();
-
-        if (Gdx.input.isKeyPressed(Input.Keys.ESCAPE)) {
-            game.setScreen(new MainMenuScreen(game));
-            dispose();
-        }
+        // Get current frame of animation for the current stateTime
+        TextureRegion currentFrame = walkAnimation.getKeyFrame(stateTime, true);
+        spriteBatch.begin();
+        spriteBatch.draw(currentFrame, xPos, 50); // Draw current frame at (50, 50)
+        xPos += 1;
+        spriteBatch.end();
     }
 
     @Override
@@ -71,5 +105,7 @@ public class Room1 implements Screen {
     @Override
     public void dispose() {
         backgroundMusic.dispose();
+        spriteBatch.dispose();
+        walkSheet.dispose();
     }
 }
