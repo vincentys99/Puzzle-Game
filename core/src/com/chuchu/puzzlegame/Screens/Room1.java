feac4a73dddeae221b2a1a04ab41b011af4cf7fd
Shiho <shihohohohohohoho@gmail.com>
2021-09-23 10:25:48 +0800

    Added Player2 class that has animation and shits

--- core/src/com/chuchu/puzzlegame/Screens/Room1.java
+++ core/src/com/chuchu/puzzlegame/Screens/Room1.java
@@ -12,8 +12,11 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.chuchu.puzzlegame.PuzzleGame;
+import com.chuchu.puzzlegame.Sprites.Player2;
 
 public class Room1 implements Screen {
+    private Player2 player;
+    private TextureRegion currentFrame;
     final PuzzleGame game;
     private static final int FRAME_COLS = 5, FRAME_ROWS = 1;
     OrthographicCamera camera;
@@ -24,10 +27,10 @@ public class Room1 implements Screen {
 
     // A variable for tracking elapsed time for the animation
     float stateTime;
-    private int xPos = 0;
+    private float xPos = 0;
+    private float yPos = 0;
     public Room1(final PuzzleGame game) {
         this.game = game;
-
         backgroundMusic = Gdx.audio.newMusic(Gdx.files.internal("music/CRSED Music Theme.mp3"));
         backgroundMusic.setLooping(true);
         backgroundMusic.setVolume(game.bgMusicVol);
@@ -35,32 +38,10 @@ public class Room1 implements Screen {
         camera = new OrthographicCamera();
         camera.setToOrtho(false, PuzzleGame.defaultWidth, PuzzleGame.defaultHeight);
 
-        walkSheet = new Texture(Gdx.files.internal("cc.png"));
+        player = new Player2();
+        currentFrame = (TextureRegion) player.walkAnimations[0].getKeyFrame(stateTime, true);
 
-        // Use the split utility method to create a 2D array of TextureRegions. This is
-        // possible because this sprite sheet contains frames of equal size and they are
-        // all aligned.
-        TextureRegion[][] tmp = TextureRegion.split(walkSheet,
-                walkSheet.getWidth() / FRAME_COLS,
-                walkSheet.getHeight() / FRAME_ROWS);
-
-        // Place the regions into a 1D array in the correct order, starting from the top
-        // left, going across first. The Animation constructor requires a 1D array.
-        TextureRegion[] walkFrames = new TextureRegion[FRAME_COLS * FRAME_ROWS];
-        int index = 0;
-        for (int i = 0; i < FRAME_ROWS; i++) {
-            for (int j = 0; j < FRAME_COLS; j++) {
-                walkFrames[index++] = tmp[i][j];
-            }
-        }
-
-        // Initialize the Animation with the frame interval and array of frames
-        walkAnimation = new Animation<TextureRegion>(0.1f, walkFrames);
-
-        // Instantiate a SpriteBatch for drawing and reset the elapsed animation
-        // time to 0
         spriteBatch = new SpriteBatch();
-        stateTime = 0f;
 
     }
 
@@ -71,17 +52,34 @@ public class Room1 implements Screen {
 
     @Override
     public void render(float delta) {
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT); // Clear screen
-        stateTime += Gdx.graphics.getDeltaTime(); // Accumulate elapsed animation time
-
-        // Get current frame of animation for the current stateTime
-        TextureRegion currentFrame = walkAnimation.getKeyFrame(stateTime, true);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        stateTime += Gdx.graphics.getDeltaTime();
         spriteBatch.begin();
-        spriteBatch.draw(currentFrame, xPos, 50); // Draw current frame at (50, 50)
-        xPos += 1;
-        spriteBatch.end();
-    }
+        handleInput();
 
+        spriteBatch.draw(currentFrame, xPos, yPos); // Draw current frame at (50, 50)
+        spriteBatch.end();
+
+    }
+    private void handleInput() {
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+            currentFrame = (TextureRegion) player.walkAnimations[0].getKeyFrame(stateTime, true);
+            xPos += 1;
+        }
+        else if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+            currentFrame = (TextureRegion) player.walkAnimations[1].getKeyFrame(stateTime, true);
+
+            xPos -= 1;
+        }
+        else if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+            currentFrame = (TextureRegion) player.walkAnimations[2].getKeyFrame(stateTime, true);
+            yPos += 1;
+        }
+        else if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+            currentFrame = (TextureRegion) player.walkAnimations[3].getKeyFrame(stateTime, true);
+            yPos -= 1;
+        }
+    }
     @Override
     public void resize(int width, int height) {
 
