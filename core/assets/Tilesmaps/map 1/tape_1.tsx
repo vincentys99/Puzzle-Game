659d2c84b51e9825d9fa6164ab07c42b1fdbd2d3
Shiho <63734164+Shiho-c@users.noreply.github.com>
2021-10-19 01:20:45 +0800

    room 1 assets

---
+++ core/assets/Tilesmaps/map 1/tape_1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="tape_1.png" tilewidth="3" tileheight="5" spacing="2" margin="1" tilecount="180" columns="20">
+ <image source="tape_1.png.png" width="100" height="62"/>
+</tileset>
