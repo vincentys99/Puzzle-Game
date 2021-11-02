f15a9feeb4b53024ea444b5fda3cfd0df5c3ae82
Eric Lim <ysfung1704@gmail.com>
2021-11-03 02:11:00 +0800

    Added pause menu v1

--- core/src/com/chuchu/puzzlegame/Screens/Room1.java
+++ core/src/com/chuchu/puzzlegame/Screens/Room1.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
@@ -39,11 +40,6 @@ import com.chuchu.puzzlegame.Sprites.Player;
 import com.chuchu.puzzlegame.Tools.Room2WorldCreator;
 import com.chuchu.puzzlegame.Tools.TileObjectClickListener;
 import com.chuchu.puzzlegame.Tools.WorldContactListener;
-
-import java.awt.*;
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
 
 public class Room1 implements Screen {
     private static boolean moveDown = false;
@@ -85,11 +81,25 @@ public class Room1 implements Screen {
     public static Stage stageTesting;
     private Skin skin;
 
+    private enum State
+    {
+        RUN,
+        PAUSE,
+        RESUME
+    }
+    private State state;
+
+    private static Texture transparentBGTexture;
+    private static Image transparentBG;
+
     public Room1(final PuzzleGame game) {
         this.game = game;
         static_game = game;
         skin = new Skin(Gdx.files.internal(Files.uiskin));
         this.timerLabel = new Label("", skin);
+        state = State.RUN;
+        transparentBGTexture = new Texture(Gdx.files.internal("images/ingame-assets/transparent.png"));
+        transparentBG = new Image(transparentBGTexture);
 
         timerLabel.setSize(30, 30);
 
@@ -137,7 +147,6 @@ public class Room1 implements Screen {
 
     public static void setup_passwordfield(String text, final String[] passwordText) {
         Gdx.input.setInputProcessor(stageTesting);
-        Image transparentBG = new Image(new Texture(Gdx.files.internal("images/ingame-assets/transparent.png")));
         transparentBG.setSize(1920, 1080);
         transparentBG.setPosition(0, 0);
         stageTesting.addActor(transparentBG);
@@ -215,7 +224,6 @@ public class Room1 implements Screen {
         int counter = 1;
         int tapeX = (Gdx.graphics.getWidth() / 2) - 160 - 80;
         Gdx.input.setInputProcessor(stageTesting);
-        Image transparentBG = new Image(new Texture(Gdx.files.internal("images/ingame-assets/transparent.png")));
         transparentBG.setSize(PuzzleGame.defaultWidth, PuzzleGame.defaultHeight);
         transparentBG.setPosition(0, 0);
         stageTesting.addActor(transparentBG);
@@ -291,32 +299,59 @@ public class Room1 implements Screen {
             } catch (IOException e) {
                 e.printStackTrace();
             }*/
-            stageTesting.clear();
-            Gdx.input.setInputProcessor(stage);
-            if (!moveable) {
+            if (stageTesting.getActors().size > 0 && state != State.PAUSE) {
+                stageTesting.clear();
+                Gdx.input.setInputProcessor(stage);
+                if (!moveable) {
+                    moveable = true;
+                    if (Door.second_password && !Door.third_password && who_counter == 0) {
+                        System.out.println("Dildo");
+                        Dialogue dialog = new Dialogue("WHO ARE YOU ? WHERE IS YOUR DILDO?!!");
+                        dialog.setup_dialogue();
 
-                moveable = true;
-                if (Door.second_password && !Door.third_password && who_counter == 0) {
-                    System.out.println("Dildo");
-                    Dialogue dialog = new Dialogue("WHO ARE YOU ? WHERE IS YOUR DILDO?!!");
-                    dialog.setup_dialogue();
-
-                    dialog.dialogueBox().addListener(new ClickListener() {
-                        @Override
-                        public void clicked(InputEvent event, float x, float y) {
-                            System.out.println("yes i clicked ur ass");
-                            stageTesting.clear();
-                            Gdx.input.setInputProcessor(Room1.stage);
-                            moveable = true;
-                            timerBool = true;
-                        }
-                    });
-                    who_counter++;
+                        dialog.dialogueBox().addListener(new ClickListener() {
+                            @Override
+                            public void clicked(InputEvent event, float x, float y) {
+                                System.out.println("yes i clicked ur ass");
+                                stageTesting.clear();
+                                Gdx.input.setInputProcessor(Room1.stage);
+                                moveable = true;
+                                timerBool = true;
+                            }
+                        });
+                        who_counter++;
+                    }
                 }
             }
+            else {
+                switch (state) {
+                    case RUN:
+                    case RESUME:
+                        state = State.PAUSE;
+                        stageTesting.clear();
+                        Gdx.input.setInputProcessor(stage);
 
+                        Table tmpTable = new Table();
+
+                        TextButton.TextButtonStyle textButtonStyle;
+                        textButtonStyle = new TextButton.TextButtonStyle();
+                        textButtonStyle.font = game.font;
+
+                        TextButton tmpText = new TextButton("PAUSED", textButtonStyle);
+                        tmpTable.add(tmpText);
+                        tmpTable.center();
+                        tmpTable.setPosition(Gdx.graphics.getWidth() / 2f, Gdx.graphics.getHeight() / 2f);
+                        stageTesting.addActor(tmpTable);
+                        break;
+                    case PAUSE:
+                        state = State.RESUME;
+                        stageTesting.clear();
+                        Gdx.input.setInputProcessor(stage);
+                        break;
+                }
+            }
         } else {
-            if (moveable) {
+            if (moveable && !(state == State.PAUSE)) {
                 float x = 0f;
                 float y = 0f;
 
@@ -364,21 +399,20 @@ public class Room1 implements Screen {
         stage.getRoot().getColor().a = 0;
         stage.getRoot().addAction(Actions.fadeOut(1f));
         backgroundMusic.play();
-
     }
 
     @Override
     public void render(float delta) {
         update(delta);
+
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
         tiledMapRenderer.setView(camera);
         tiledMapRenderer.render();
         //=============================================================//
         //  the line below is used to display the lines of the objects //
         //=============================================================//
-        b2dr.render(world, camera.combined);
+        //b2dr.render(world, camera.combined);
 
         game.batch.setProjectionMatrix(camera.combined);
         game.batch.begin();
@@ -417,6 +451,13 @@ public class Room1 implements Screen {
         stage.act();
         stage.draw();
 
+        if (state == State.PAUSE) {
+            pause();
+        }
+        if (state == State.RESUME) {
+            resume();
+            state = State.RUN;
+        }
     }
 
     @Override
@@ -428,12 +469,21 @@ public class Room1 implements Screen {
 
     @Override
     public void pause() {
+        stageTesting.act();
+        stageTesting.draw();
 
+        game.batch.begin();
+        game.batch.draw(transparentBGTexture, 0, 0);
+        game.batch.end();
+
+        if (backgroundMusic.isPlaying()) {
+            backgroundMusic.pause();
+        }
     }
 
     @Override
     public void resume() {
-
+        backgroundMusic.play();
     }
 
     @Override
