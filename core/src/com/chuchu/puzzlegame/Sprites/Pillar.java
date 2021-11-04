92b2381a5f96d7e10743abc413810c971ba80f14
Shiho <63734164+Shiho-c@users.noreply.github.com>
2021-11-05 00:49:02 +0800

    pillar

--- core/src/com/chuchu/puzzlegame/Sprites/Pillar.java
+++ core/src/com/chuchu/puzzlegame/Sprites/Pillar.java
@@ -1,29 +1,19 @@
 package com.chuchu.puzzlegame.Sprites;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.maps.MapGroupLayer;
-import com.badlogic.gdx.maps.MapLayer;
-import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.math.Rectangle;
-import com.badlogic.gdx.physics.box2d.Body;
-import com.badlogic.gdx.physics.box2d.Filter;
-import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.World;
-
-import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.chuchu.puzzlegame.Screens.Room1;
 import com.chuchu.puzzlegame.Tools.TileObjectClickListener;
-import com.rafaskoberg.gdx.typinglabel.TypingLabel;
 
 
-public class Door extends InteractiveTileObject {
+public class Pillar extends InteractiveTileObject {
     public static boolean first_password = false;
     public static boolean second_password = false;
     public static boolean third_password = false;
-    public Door(World world, TiledMap map, Rectangle bounds, float unitScale, Stage stage) {
+    public Pillar(World world, TiledMap map, Rectangle bounds, float unitScale, Stage stage) {
         super(world, map, bounds, unitScale, stage);
         fixture.setUserData(this);
         setCategoryFilter(Room1.DOOR_BIT);
@@ -32,7 +22,7 @@ public class Door extends InteractiveTileObject {
 
     @Override
     public void onHit() {
-        Gdx.app.log("Door", "HIT!");
+        Gdx.app.log("Pillar", "HIT!");
         if( TileObjectClickListener.doorUnlocked) {
             setCategoryFilter(Room1.DESTROYED_BIT);
             onEndContact();
@@ -42,13 +32,13 @@ public class Door extends InteractiveTileObject {
 
     @Override
     public void onBeginContact() {
-        Gdx.app.log("Door", "Begin contact");
-        this.setName("Door");
+        Gdx.app.log("Pillar", "Begin contact");
+        this.setName("Pillar");
         addActor();
 
     }
     @Override
     public void onEndContact() {
-        Gdx.app.log("Door", "End contact");
+        Gdx.app.log("Pillar", "End contact");
     }
 }
