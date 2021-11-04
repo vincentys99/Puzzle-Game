24d1e94b184a2787468cfcc29857200d426dd256
Eric Lim <ysfung1704@gmail.com>
2021-11-04 19:22:07 +0800

    Added more npc

---
+++ core/assets/Tilesmaps/map 2/Vincent_14x26.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Vincent_14x26" tilewidth="3" tileheight="5" tilecount="20" columns="4">
+ <image source="Vincent_14x26.png" width="14" height="26"/>
+</tileset>
